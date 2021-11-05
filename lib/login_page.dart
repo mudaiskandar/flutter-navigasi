b13cc713f1efee7fd86e6360c705241f673c4ede
PC-USER <mudaiskandarmuda@gmail.com>
2021-11-05 16:32:58 +0700

    first project

---
+++ lib/login_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:latihan_navigasi/main_page.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          child: Text("LOGINN"),
+          onPressed: () {
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) {
+              return MainPage();
+            }));
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:latihan_navigasi/second_page.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Main Pagee"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Go To Second Page"),
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return SecondPage();
+            }));
+            
+          },
+        ),
+      ),
+    );
+  }
+}
